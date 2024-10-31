fe54ca68b5de338132774adb4e8bb5f70b516bd6
KrasyukME <Tyuiu.KrasyukME@outlook.com>
2024-10-31 20:29:23 +0500

    Add project files.

---
+++ Tyuiu.KrasyukME.Sprint3.Task0.V13.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.KrasyukME.Sprint3.Task0.V13.Lib
+{
+    public class DataService : ISprint3Task0V13
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double S = 0;
+            int i;
+            for (i = startValue; i <= stopValue; i++)
+            {
+                S = S + ((Math.Pow(value, i)) + (0.5)) * Math.Cos(i);
+
+            }
+
+            return Math.Round(S, 3);
+        }
+    }
+}
---
+++ Tyuiu.KrasyukME.Sprint3.Task0.V13/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.KrasyukME.Sprint3.Task0.V13.Lib;
+namespace Tyuiu.KrasyukME.Sprint3.Task0.V13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                *");
+            Console.WriteLine("* Тема:                                                                    *");
+            Console.WriteLine("* Задание #0                                                               *");
+            Console.WriteLine("* Вариант #13                                                              *");
+            Console.WriteLine("* Выполнила: Красюк Мария Евгеньевна | АСОиУБ-24-1                         *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            DataService ds = new DataService();
+
+
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 20;
+            Console.WriteLine("Пременная a = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Сумма ряда: " + ds.GetSumSeries(value, startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
